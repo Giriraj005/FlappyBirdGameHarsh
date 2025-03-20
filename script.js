dd4f84850bee65fe0cdc3bc61b1983e930c688f9
Giriraj005 <hpareek017@gmail.com>
2025-03-20 22:41:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+let currentPlayer = 'red';
+let diceRoll = 0;
+
+function rollDice() {
+    diceRoll = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("diceResult").innerText = `🎲 Rolled: ${diceRoll}`;
+    movePlayer(currentPlayer, diceRoll);
+    switchTurn();
+}
+
+function movePlayer(player, steps) {
+    alert(`${player} moves ${steps} steps!`);
+}
+
+function switchTurn() {
+    const players = ['red', 'blue', 'green', 'yellow'];
+    let index = players.indexOf(currentPlayer);
+    currentPlayer = players[(index + 1) % players.length];
+    alert(`Now it's ${currentPlayer}'s turn!`);
+}
